590304656d18ef9e51270ae49027362e61d07933
Diana Lee <dianalee.g.842@gmail.com>
2023-07-28 20:13:45 -0700

    Add files via upload

---
+++ studentLCDLEDPiezo_Mario.ino
@@ -0,0 +1,577 @@
+#include <LiquidCrystal_I2C.h>
+
+#define PIN_BUTTON 2
+#define PIN_AUTOPLAY 1
+#define PIN_READWRITE 10
+#define PIN_CONTRAST 12
+
+// DEFINE LED PIN HERE - CIC
+// DEFINE BUZZER PIN HERE - CIC
+
+#define SPRITE_RUN1 1
+#define SPRITE_RUN2 2
+#define SPRITE_JUMP 3
+#define SPRITE_JUMP_UPPER '.'         // Use the '.' character for the head
+#define SPRITE_JUMP_LOWER 4
+#define SPRITE_TERRAIN_EMPTY ' '      // User the ' ' character
+#define SPRITE_TERRAIN_SOLID 5
+#define SPRITE_TERRAIN_SOLID_RIGHT 6
+#define SPRITE_TERRAIN_SOLID_LEFT 7
+
+#define HERO_HORIZONTAL_POSITION 1    // Horizontal position of hero on screen
+
+#define TERRAIN_WIDTH 16
+#define TERRAIN_EMPTY 0
+#define TERRAIN_LOWER_BLOCK 1
+#define TERRAIN_UPPER_BLOCK 2
+
+#define HERO_POSITION_OFF 0          // Hero is invisible
+#define HERO_POSITION_RUN_LOWER_1 1  // Hero is running on lower row (pose 1)
+#define HERO_POSITION_RUN_LOWER_2 2  //                              (pose 2)
+
+#define HERO_POSITION_JUMP_1 3       // Starting a jump
+#define HERO_POSITION_JUMP_2 4       // Half-way up
+#define HERO_POSITION_JUMP_3 5       // Jump is on upper row
+#define HERO_POSITION_JUMP_4 6       // Jump is on upper row
+#define HERO_POSITION_JUMP_5 7       // Jump is on upper row
+#define HERO_POSITION_JUMP_6 8       // Jump is on upper row
+#define HERO_POSITION_JUMP_7 9       // Half-way down
+#define HERO_POSITION_JUMP_8 10      // About to land
+
+#define HERO_POSITION_RUN_UPPER_1 11 // Hero is running on upper row (pose 1)
+#define HERO_POSITION_RUN_UPPER_2 12 //                              (pose 2)
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+static char terrainUpper[TERRAIN_WIDTH + 1];
+static char terrainLower[TERRAIN_WIDTH + 1];
+static bool buttonPushed = false;
+
+void initializeGraphics(){
+  static byte graphics[] = {
+    // Run position 1
+    B01100,
+    B01100,
+    B00000,
+    B01110,
+    B11100,
+    B01100,
+    B11010,
+    B10011,
+    // Run position 2
+    B01100,
+    B01100,
+    B00000,
+    B01100,
+    B01100,
+    B01100,
+    B01100,
+    B01110,
+    // Jump
+    B01100,
+    B01100,
+    B00000,
+    B11110,
+    B01101,
+    B11111,
+    B10000,
+    B00000,
+    // Jump lower
+    B11110,
+    B01101,
+    B11111,
+    B10000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    // Ground
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    // Ground right
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    // Ground left
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+  };
+  int i;
+  // Skip using character 0, this allows lcd.print() to be used to
+  // quickly draw multiple characters
+  for (i = 0; i < 7; ++i) {
+	  lcd.createChar(i + 1, &graphics[i * 8]);
+  }
+  for (i = 0; i < TERRAIN_WIDTH; ++i) {
+    terrainUpper[i] = SPRITE_TERRAIN_EMPTY;
+    terrainLower[i] = SPRITE_TERRAIN_EMPTY;
+  }
+}
+
+// Slide the terrain to the left in half-character increments
+//
+void advanceTerrain(char* terrain, byte newTerrain){
+  for (int i = 0; i < TERRAIN_WIDTH; ++i) {
+    char current = terrain[i];
+    char next = (i == TERRAIN_WIDTH-1) ? newTerrain : terrain[i+1];
+    switch (current){
+      case SPRITE_TERRAIN_EMPTY:
+        terrain[i] = (next == SPRITE_TERRAIN_SOLID) ? SPRITE_TERRAIN_SOLID_RIGHT : SPRITE_TERRAIN_EMPTY;
+        break;
+      case SPRITE_TERRAIN_SOLID:
+        terrain[i] = (next == SPRITE_TERRAIN_EMPTY) ? SPRITE_TERRAIN_SOLID_LEFT : SPRITE_TERRAIN_SOLID;
+        break;
+      case SPRITE_TERRAIN_SOLID_RIGHT:
+        terrain[i] = SPRITE_TERRAIN_SOLID;
+        break;
+      case SPRITE_TERRAIN_SOLID_LEFT:
+        terrain[i] = SPRITE_TERRAIN_EMPTY;
+        break;
+    }
+  }
+}
+
+bool drawHero(byte position, char* terrainUpper, char* terrainLower, unsigned int score) {
+  bool collide = false;
+  char upperSave = terrainUpper[HERO_HORIZONTAL_POSITION];
+  char lowerSave = terrainLower[HERO_HORIZONTAL_POSITION];
+  byte upper, lower;
+  switch (position) {
+    case HERO_POSITION_OFF:
+      upper = lower = SPRITE_TERRAIN_EMPTY;
+      break;
+    case HERO_POSITION_RUN_LOWER_1:
+      upper = SPRITE_TERRAIN_EMPTY;
+      lower = SPRITE_RUN1;
+      break;
+    case HERO_POSITION_RUN_LOWER_2:
+      upper = SPRITE_TERRAIN_EMPTY;
+      lower = SPRITE_RUN2;
+      break;
+    case HERO_POSITION_JUMP_1:
+    case HERO_POSITION_JUMP_8:
+      upper = SPRITE_TERRAIN_EMPTY;
+      lower = SPRITE_JUMP;
+      break;
+    case HERO_POSITION_JUMP_2:
+    case HERO_POSITION_JUMP_7:
+      upper = SPRITE_JUMP_UPPER;
+      lower = SPRITE_JUMP_LOWER;
+      break;
+    case HERO_POSITION_JUMP_3:
+    case HERO_POSITION_JUMP_4:
+    case HERO_POSITION_JUMP_5:
+    case HERO_POSITION_JUMP_6:
+      upper = SPRITE_JUMP;
+      lower = SPRITE_TERRAIN_EMPTY;
+      break;
+    case HERO_POSITION_RUN_UPPER_1:
+      upper = SPRITE_RUN1;
+      lower = SPRITE_TERRAIN_EMPTY;
+      break;
+    case HERO_POSITION_RUN_UPPER_2:
+      upper = SPRITE_RUN2;
+      lower = SPRITE_TERRAIN_EMPTY;
+      break;
+  }
+  if (upper != ' ') {
+    terrainUpper[HERO_HORIZONTAL_POSITION] = upper;
+    collide = (upperSave == SPRITE_TERRAIN_EMPTY) ? false : true;
+  }
+  if (lower != ' ') {
+    terrainLower[HERO_HORIZONTAL_POSITION] = lower;
+    collide |= (lowerSave == SPRITE_TERRAIN_EMPTY) ? false : true;
+  }
+  
+  byte digits = (score > 9999) ? 5 : (score > 999) ? 4 : (score > 99) ? 3 : (score > 9) ? 2 : 1;
+  
+  // Draw the scene
+  terrainUpper[TERRAIN_WIDTH] = '\0';
+  terrainLower[TERRAIN_WIDTH] = '\0';
+  char temp = terrainUpper[16-digits];
+  terrainUpper[16-digits] = '\0';
+  lcd.setCursor(0,0);
+  lcd.print(terrainUpper);
+  terrainUpper[16-digits] = temp;  
+  lcd.setCursor(0,1);
+  lcd.print(terrainLower);
+  
+  lcd.setCursor(16 - digits,0);
+  lcd.print(score);
+
+  terrainUpper[HERO_HORIZONTAL_POSITION] = upperSave;
+  terrainLower[HERO_HORIZONTAL_POSITION] = lowerSave;
+  return collide;
+}
+
+// Handle the button push as an interrupt
+void buttonPush() {
+  buttonPushed = true;
+}
+
+/*
+  Arduino Mario Bros Tunes
+  With Piezo Buzzer and PWM
+  by: Dipto Pratyaksa
+  last updated: 31/3/13
+*/
+
+/*************************************************
+ * Public Constants
+ *************************************************/
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+#define melodyPin 3
+//Mario main theme melody
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7, 
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0, 
+
+  NOTE_C7, 0, 0, NOTE_G6, 
+  0, 0, NOTE_E6, 0, 
+  0, NOTE_A6, 0, NOTE_B6, 
+  0, NOTE_AS6, NOTE_A6, 0, 
+
+  NOTE_G6, NOTE_E7, NOTE_G7, 
+  NOTE_A7, 0, NOTE_F7, NOTE_G7, 
+  0, NOTE_E7, 0,NOTE_C7, 
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6, 
+  0, 0, NOTE_E6, 0, 
+  0, NOTE_A6, 0, NOTE_B6, 
+  0, NOTE_AS6, NOTE_A6, 0, 
+
+  NOTE_G6, NOTE_E7, NOTE_G7, 
+  NOTE_A7, 0, NOTE_F7, NOTE_G7, 
+  0, NOTE_E7, 0,NOTE_C7, 
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12, 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12, 
+
+  12, 12, 12, 12,
+  12, 12, 12, 12, 
+  12, 12, 12, 12, 
+  12, 12, 12, 12, 
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+
+//
+//Underworld melody
+int underworld_melody[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4, 
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4, 
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4, 
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4,NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+//Underwolrd tempo
+int underworld_tempo[] = {
+  12, 12, 12, 12, 
+  12, 12, 6,
+  3,
+  12, 12, 12, 12, 
+  12, 12, 6,
+  3,
+  12, 12, 12, 12, 
+  12, 12, 6,
+  3,
+  12, 12, 12, 12, 
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18,18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
+int song = 0;
+
+void sing(){      
+
+      static byte heroPos = HERO_POSITION_RUN_LOWER_1;
+      static byte newTerrainType = TERRAIN_EMPTY;
+      static byte newTerrainDuration = 1;
+      static bool playing = false;
+      static bool blink = false;
+      static unsigned int distance = 0;
+
+    // ******** PIEZO CODE *************
+
+     Serial.println(" 'Mario Theme'");
+     int size = sizeof(melody) / sizeof(int);
+
+      for (int thisNote = 0; thisNote < size; thisNote++) {
+
+        // to calculate the note duration, take one second
+        // divided by the note type.
+        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+        int noteDuration = 1000/tempo[thisNote];
+        if(playing){
+        buzz(melodyPin, melody[thisNote],noteDuration);
+
+        // to distinguish the notes, set a minimum time between them.
+        // the note's duration + 30% seems to work well:
+        int pauseBetweenNotes = noteDuration * 0.8;
+        delay(pauseBetweenNotes);
+
+        // stop the tone playing:
+        buzz(melodyPin, 0,noteDuration);
+        }
+  
+
+       // ******* LCD CODE *************
+  
+      if (!playing) {
+        drawHero((blink) ? HERO_POSITION_OFF : heroPos, terrainUpper, terrainLower, distance >> 3);
+        if (blink) {
+          lcd.setCursor(0,0);
+          // PRINT TO THE LCD "Press Start" HERE - CIC
+        }
+        delay(100);
+        blink = !blink;
+        if (buttonPushed) {
+          initializeGraphics();
+          heroPos = HERO_POSITION_RUN_LOWER_1;
+          playing = true;
+          buttonPushed = false;
+          distance = 0;
+        }
+        return;
+      }
+
+      digitalWrite(LED_PIN, HIGH);
+    // Shift the terrain to the left
+      advanceTerrain(terrainLower, newTerrainType == TERRAIN_LOWER_BLOCK ? SPRITE_TERRAIN_SOLID : SPRITE_TERRAIN_EMPTY);
+      advanceTerrain(terrainUpper, newTerrainType == TERRAIN_UPPER_BLOCK ? SPRITE_TERRAIN_SOLID : SPRITE_TERRAIN_EMPTY);
+      
+      // Make new terrain to enter on the right
+      if (--newTerrainDuration == 0) {
+        if (newTerrainType == TERRAIN_EMPTY) {
+          newTerrainType = (random(3) == 0) ? TERRAIN_UPPER_BLOCK : TERRAIN_LOWER_BLOCK;
+          newTerrainDuration = 2 + random(10);
+        } else {
+          newTerrainType = TERRAIN_EMPTY;
+          newTerrainDuration = 10 + random(10);
+        }
+      }
+    
+      if (buttonPushed) {
+        if (heroPos <= HERO_POSITION_RUN_LOWER_2) heroPos = HERO_POSITION_JUMP_1;
+        buttonPushed = false;
+      }  
+
+      if (drawHero(heroPos, terrainUpper, terrainLower, distance >> 3)) {
+        playing = false; // The hero collided with something. Too bad.
+      } else {
+        if (heroPos == HERO_POSITION_RUN_LOWER_2 || heroPos == HERO_POSITION_JUMP_8) {
+          heroPos = HERO_POSITION_RUN_LOWER_1;
+        } else if ((heroPos >= HERO_POSITION_JUMP_3 && heroPos <= HERO_POSITION_JUMP_5) && terrainLower[HERO_HORIZONTAL_POSITION] != SPRITE_TERRAIN_EMPTY) {
+          heroPos = HERO_POSITION_RUN_UPPER_1;
+        } else if (heroPos >= HERO_POSITION_RUN_UPPER_1 && terrainLower[HERO_HORIZONTAL_POSITION] == SPRITE_TERRAIN_EMPTY) {
+          heroPos = HERO_POSITION_JUMP_5;
+        } else if (heroPos == HERO_POSITION_RUN_UPPER_2) {
+          heroPos = HERO_POSITION_RUN_UPPER_1;
+        } else {
+          ++heroPos;
+        }
+        ++distance;
+          digitalWrite(LED_PIN, LOW);
+
+        digitalWrite(PIN_AUTOPLAY, terrainLower[HERO_HORIZONTAL_POSITION + 2] == SPRITE_TERRAIN_EMPTY ? HIGH : LOW);
+    }
+  }
+}
+
+void buzz(int targetPin, long frequency, long length) {
+
+  // TURN ON YOUR LED HERE - CIC
+
+  long delayValue = 1000000/frequency/2; // calculate the delay value between transitions
+  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
+  //// there are two phases to each cycle
+  long numCycles = frequency * length/ 1000; // calculate the number of cycles for proper timing
+  //// multiply frequency, which is really cycles per second, by the number of seconds to 
+  //// get the total number of cycles to produce
+  for (long i=0; i < numCycles; i++){ // for the calculated length of time...
+    digitalWrite(targetPin,HIGH); // write the buzzer pin high to push out the diaphram
+    delayMicroseconds(delayValue); // wait for the calculated delay value
+    digitalWrite(targetPin,LOW); // write the buzzer pin low to pull back the diaphram
+    delayMicroseconds(delayValue); // wait again or the calculated delay value
+  }
+
+  // TURN OFF YOUR LED HERE - CIC
+
+}
+
+void setup(){
+  pinMode(PIN_READWRITE, OUTPUT);
+  digitalWrite(PIN_READWRITE, LOW);
+  pinMode(PIN_CONTRAST, OUTPUT);
+  digitalWrite(PIN_CONTRAST, LOW);
+  pinMode(PIN_BUTTON, INPUT);
+  digitalWrite(PIN_BUTTON, HIGH);
+  pinMode(PIN_AUTOPLAY, OUTPUT);
+  digitalWrite(PIN_AUTOPLAY, HIGH);
+  
+  // Digital pin 2 maps to interrupt 0
+  attachInterrupt(0/*PIN_BUTTON*/, buttonPush, FALLING);
+  
+  initializeGraphics();
+
+  // INITIALIZE YOUR LCD HERE - CIC
+  // TURN ON LCD BACKLIGHT HERE - CIC 
+
+  // SET PINMODE FOR LED PIN HERE - CIC
+  // SET PINMODE FOR BUZZER PIN HERE - CIC
+
+}
+
+void loop(){
+  
+  // CALL THE sing FUNCTION HERE - CIC 
+  // PUT IN A SHORT DELAY HERE - CIC
+
+}
